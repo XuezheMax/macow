f5a8b464359cd0b319c5838b69e242d10d63591b
Max Ma <xuezhe.ma@gmail.com>
2018-10-24 00:09:12 -0400

    utils

---
+++ macow/utils.py
@@ -0,0 +1,17 @@
+__author__ = 'max'
+
+import torch
+
+
+def norm(p: torch.Tensor, dim: int):
+    """Computes the norm over all dimensions except dim"""
+    if dim is None:
+        return p.norm()
+    elif dim == 0:
+        output_size = (p.size(0),) + (1,) * (p.dim() - 1)
+        return p.contiguous().view(p.size(0), -1).norm(dim=1).view(*output_size)
+    elif dim == p.dim() - 1:
+        output_size = (1,) * (p.dim() - 1) + (p.size(-1),)
+        return p.contiguous().view(-1, p.size(-1)).norm(dim=0).view(*output_size)
+    else:
+        return norm(p.transpose(0, dim), 0).transpose(0, dim)
