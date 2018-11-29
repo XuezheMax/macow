41508dff4b3a75cae2d27cb6d3c6358cd39d1412
Max Ma <xuezhe.ma@gmail.com>
2018-11-29 15:45:33 -0500

    final

--- macow/flows/macow.py
+++ macow/flows/macow.py
@@ -53,41 +53,53 @@ class MaCowStep(Flow):
     """
     def __init__(self, in_channels, kernel_size, hidden_channels, scale=True, inverse=False, dropout=0.0):
         super(MaCowStep, self).__init__(inverse)
-        # self.coupling = NICE(in_channels, hidden_channels=hidden_channels, scale=scale, inverse=inverse, dropout=dropout)
-        self.coupling = GlowStep(in_channels, hidden_channels=hidden_channels, scale=scale, inverse=inverse, dropout=dropout)
+        self.actnorm = ActNorm2dFlow(in_channels, inverse=inverse)
+        self.conv1x1 = Conv1x1Flow(in_channels, inverse=inverse)
         num_units = 2
         units = [MCF(in_channels, kernel_size, scale=scale, inverse=inverse) for _ in range(num_units)]
         self.units = nn.ModuleList(units)
+        self.coupling = NICE(in_channels, hidden_channels=hidden_channels, scale=scale, inverse=inverse, dropout=dropout)
+        # self.coupling = GlowStep(in_channels, hidden_channels=hidden_channels, scale=scale, inverse=inverse, dropout=dropout)
 
     @overrides
     def forward(self, input: torch.Tensor, h=None) -> Tuple[torch.Tensor, torch.Tensor]:
-        # logdet_accum = input.new_zeros(input.size(0))
-        # out = input
-        out, logdet_accum = self.coupling.forward(input, h=h)
+        out, logdet_accum = self.actnorm.forward(input, h=h)
+        out, logdet = self.conv1x1.forward(out, h=h)
+        logdet_accum = logdet_accum + logdet
+
         for unit in self.units:
             out, logdet = unit.forward(out, h=h)
             logdet_accum = logdet_accum + logdet
+
+        out, logdet_accum = self.coupling.forward(out, h=h)
         return out, logdet_accum
 
     @overrides
     def backward(self, input: torch.Tensor, h=None) -> Tuple[torch.Tensor, torch.Tensor]:
-        logdet_accum = input.new_zeros(input.size(0))
-        out = input
+        out, logdet_accum = self.coupling.backward(input)
+
         for unit in reversed(self.units):
             out, logdet = unit.backward(out, h=h)
             logdet_accum = logdet_accum + logdet
-        out, logdet = self.coupling.backward(out, h=h)
+
+        out, logdet = self.conv1x1.backward(out, h=h)
+        logdet_accum = logdet_accum + logdet
+        out, logdet = self.actnorm.backward(out, h=h)
         logdet_accum = logdet_accum + logdet
         return out, logdet_accum
 
     @overrides
     def init(self, data, h=None, init_scale=1.0) -> Tuple[torch.Tensor, torch.Tensor]:
-        # logdet_accum = data.new_zeros(data.size(0))
-        # out = data
-        out, logdet_accum = self.coupling.init(data, h=h, init_scale=init_scale)
+        out, logdet_accum = self.actnorm.init(data, h=h, init_scale=init_scale)
+        out, logdet = self.conv1x1.init(out, h=h, init_scale=init_scale)
+        logdet_accum = logdet_accum + logdet
+
         for unit in self.units:
             out, logdet = unit.init(out, h=h, init_scale=init_scale)
             logdet_accum = logdet_accum + logdet
+
+        out, logdet = self.coupling.init(out, h=h, init_scale=init_scale)
+        logdet_accum = logdet_accum + logdet
         return out, logdet_accum
 
 
