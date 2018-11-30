124ccb3786dab2ba52f113937127028e4800b530
Max Ma <xuezhe.ma@gmail.com>
2018-11-29 21:08:53 -0500

    tanh

--- macow/flows/conv.py
+++ macow/flows/conv.py
@@ -191,8 +191,8 @@ class MaskedConvFlow(Flow):
         scale = None
         if self.scale:
             mu, log_scale = mu.chunk(2, dim=1)
-            scale = log_scale.add_(2.).sigmoid_()
-            # scale = log_scale.tanh() + 1.0
+            # scale = log_scale.add_(2.).sigmoid_()
+            scale = log_scale.tanh_() + 1.0
         return mu, scale
 
     def init_net(self, x, h=None, init_scale=1.0):
@@ -202,8 +202,8 @@ class MaskedConvFlow(Flow):
         scale = None
         if self.scale:
             mu, log_scale = mu.chunk(2, dim=1)
-            scale = log_scale.add_(2.).sigmoid_()
-            # scale = log_scale.tanh_().add_(1.0)
+            # scale = log_scale.add_(2.).sigmoid_()
+            scale = log_scale.tanh_().add_(1.0)
         return mu, scale
 
     @overrides
