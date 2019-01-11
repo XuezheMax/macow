aa39f98bde1b618a171a0a75c44e494cf8d00145
Max Ma <xuezhe.ma@gmail.com>
2019-01-10 21:36:19 -0500

    bug

--- macow/flows/dequant.py
+++ macow/flows/dequant.py
@@ -27,7 +27,7 @@ class DeQuantFlow(Flow):
             for level in range(1, levels):
                 out_plane = min(planes * 2, 96)
                 out_planes.append(out_plane)
-                layers.append(('down%d' % level, Conv2dWeightNorm(planes, out_planes, 3, 2, 1, bias=True)))
+                layers.append(('down%d' % level, Conv2dWeightNorm(planes, out_plane, 3, 2, 1, bias=True)))
                 layers.append(('elu%d' % level, nn.ELU(inplace=True)))
                 planes = out_plane
                 layers.append(('resnet%d' % level, ResNet(planes, [planes, planes], [1, 1])))
