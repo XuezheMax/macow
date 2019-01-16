cdb3fd287400565c03938e23f6f812b46e5d734c
Max Ma <xuezhe.ma@gmail.com>
2019-01-16 14:27:16 -0500

    scale=False for bottom

--- macow/flows/macow.py
+++ macow/flows/macow.py
@@ -111,7 +111,7 @@ class MaCowBottomBlock(Flow):
     """
     def __init__(self, num_steps, in_channels, kernel_size, s_channels, dilation, inverse=False):
         super(MaCowBottomBlock, self).__init__(inverse)
-        steps = [MaCowStep(in_channels, kernel_size, None, s_channels, dilation, scale=True, inverse=inverse) for _ in range(num_steps)]
+        steps = [MaCowStep(in_channels, kernel_size, None, s_channels, dilation, scale=False, inverse=inverse) for _ in range(num_steps)]
         self.steps = nn.ModuleList(steps)
 
     @overrides
