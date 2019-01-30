0f0a1c8b7ba6f72fdc71812bb49cd1a701bf8d83
Max Ma <xuezhe.ma@gmail.com>
2019-01-29 23:47:34 -0500

    gate for mcf

--- macow/flows/conv.py
+++ macow/flows/conv.py
@@ -164,7 +164,7 @@ class Conv1x1WeightNormFlow(Flow):
 
 
 def gate(x1, x2):
-    return x1 * x2.sigmoid_()
+    return x1 * x2.sigmoid()
 
 
 class MCFBlock(nn.Module):
@@ -205,7 +205,7 @@ class MaskedConvFlow(Flow):
                 hidden_channels = 4 * in_channels
             else:
                 hidden_channels = min(2 * in_channels, 512)
-        out_channels = in_channels
+        out_channels = in_channels * 2
         if scale:
             out_channels = out_channels * 2
         self.kernel_size = _pair(kernel_size)
@@ -217,19 +217,27 @@ class MaskedConvFlow(Flow):
             self.s_conv = Conv2dWeightNorm(s_channels, hidden_channels, kernel_size, bias=True, padding=self.net.padding)
 
     def calc_mu_and_scale(self, x: torch.Tensor, s=None):
-        mu = self.net(x, s=s)
+        c = self.net(x, s=s)
         scale = None
         if self.scale:
-            mu, log_scale = mu.chunk(2, dim=1)
+            mu1, mu2, log_scale1, log_scale2 = c.chunk(4, dim=1)
+            log_scale = gate(log_scale1, log_scale2)
             scale = log_scale.add_(2.).sigmoid_()
+        else:
+            mu1, mu2 = c.chunk(2, dim=1)
+        mu = gate(mu1, mu2)
         return mu, scale
 
     def init_net(self, x, s=None, init_scale=1.0):
-        mu = self.net.init(x, s=s, init_scale=init_scale)
+        c = self.net.init(x, s=s, init_scale=init_scale)
         scale = None
         if self.scale:
-            mu, log_scale = mu.chunk(2, dim=1)
+            mu1, mu2, log_scale1, log_scale2 = c.chunk(4, dim=1)
+            log_scale = gate(log_scale1, log_scale2)
             scale = log_scale.add_(2.).sigmoid_()
+        else:
+            mu1, mu2 = c.chunk(2, dim=1)
+        mu = gate(mu1, mu2)
         return mu, scale
 
     @overrides
