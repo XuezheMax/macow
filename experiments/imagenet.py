9956e18976ebdb71cdd1825433ff110e2f8a2e11
Max Ma <xuezhe.ma@gmail.com>
2019-02-02 17:14:38 -0500

    imagenet64

--- experiments/imagenet.py
+++ experiments/imagenet.py
@@ -79,7 +79,7 @@ np.random.shuffle(train_index)
 test_index = np.arange(len(test_data))
 
 train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, pin_memory=True)
-test_loader = DataLoader(test_data, batch_size=250, shuffle=False, num_workers=args.workers, pin_memory=True)
+test_loader = DataLoader(test_data, batch_size=100, shuffle=False, num_workers=args.workers, pin_memory=True)
 batch_steps = args.batch_steps
 
 print(len(train_index))
